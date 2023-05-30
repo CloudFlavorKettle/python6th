aca1768a11f9866605ad1181db8020d970ef0c48
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-05-30 11:24:23 +0900

    5/30 JS 실습

---
+++ default.js
@@ -0,0 +1,12 @@
+document.write('Hello world');
+document.write('<h1>Welcome to JS Program</h1>');
+document.write('<h2>Welcome to JS Program</h2>');
+
+console.log('Welcome to JS Program');
+console.info('Welcome to JS Program');
+console.warn('Welcome to JS Program');
+console.error('Welcome to JS Program');
+
+alert('Welcome to JS Program');
+var a = prompt('Welcome to JS Program');
+console.log(0);
