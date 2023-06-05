cd212b53f9745e39dc2c4ec5c99a0c9f64a6ebe5
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-05 13:43:54 +0900

    멤버 연산 실습

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-a = 10
-b = 15
+# 멤버 in 연산자
 
-print('a: ', bin(a))
-print('b: ', bin(b))
-print('a & b: ', a & b)
-print('a<<2: ', a << 2)
-print('a>>2: ', a >> 2)
+st1 = "Welcome to 멋쟁이 사자"
+print("to" in st1)
+
+st2 = "Welcome top 멋쟁이 사자"
+print("to" in st2)
+
+st3 = "Welcome to 멋쟁이 사자"
+print("subs" in st3)
