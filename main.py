c2d58293e34adc9b05c2f5e1c06d76dc615828a6
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-05 15:33:06 +0900

    입력 함수 실습

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-data = [10, 20, -50, 21.3, 'LikeLion']
-print(data)
-
-print("Like", "Share", "Subscribe", sep='')
-print("Like", "Share", "Subscribe", sep='***')
-
-print("Like", "Share", "Subscribe", sep='***', end='\t')
-print("Like", "Share", "Subscribe", sep='***', end='\n')
-
-m = 40
-print("value: ", m)
-
-name = "배정만"
-age = 42
-print("My name is", name, "and My age is", age, sep=' ')
-
-print("Welcome", end='\t')
-print("to", end='\t')
-print("LikeLion")
-
+# 입력 함수 실습
+# name = input()
+#
+# print(name)
+#
+# name = input("Your Name: ")
+mobile = input("Enter Your Mobile Number: ")
+mb = int(mobile)
+print(mb, type(mb))
