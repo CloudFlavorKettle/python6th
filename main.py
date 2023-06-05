174b8974fa6c521de080e0f56046984cac42d9e0
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-05 14:22:23 +0900

    명시적 타입 변환 정수(int)-문자열(str)

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-# 암시적 타입 변환
-a = 5
-b = 2
-print(b, type(b))
-value = a / b
-print(value)
-print(type(value))
-
-x = 10
-y = 5.5
-total = x + y
-print(total)
-print(type(total))
-
-j = "Hello"
-k = " like lion"
-p = j + k
-print(p)
-print(type(p))
-
+# 명시적 타입 변환
 q = 20
 u = '10'
-r = q + u # 오류
-print(r)
-
+print(type(u))
+r = str(q)  + u
+print(r, type(r))
