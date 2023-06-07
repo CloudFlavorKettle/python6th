5083267bb1497395777e17ae8baf849360ea7328
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-07 10:21:16 +0900

    while 실습

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-# x = 5
-# if x > 3:
-#     print("x는 3보다 크다.")
-# 
-# age = 18
-# if age >= 18:
-#     print("성인입니다.")
+# i = 5
+# while i < 6:
+#     i += 1
+#     print("i: ", i)
 # else:
-#     print("미성년자입니다.")
-
-# score = 85
-# if score >= 90:
-#     print("A 학점")
+#     print("else")
+#
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
 # else:
-#     if score >= 80:
-#         print("B 학점")
-#     else:
-#         if score >= 70:
-#             print("C 학점")
-#         else:
-#             print("D 학점")
+#     print("While 조건이 거짓이므로 Else 부분 실행됨")
+# print("코드 종료")
+#
+# while True:
+#     print("멋쟁이 사자")
+# print("코드 종료")
 
-# a = int(input("Enter Number Greater then or equal to 2:"))
-# if a >= 3:
-#     print("Correct!! You have Entered: ", a)
-# else:
-#     print("Wrong!! You have Entered: ", a)
-
-day = input("요일을 입력하세요: ")
-if day == "Mon":
-    print("오늘은 월요일")
-elif day == "Tue":
-    print("화요일")
-elif day == "Wed":
-    print("수요일")
-else:
-    print("휴일")
+i = 0
+while True:
+    i += 1
+    print(i)
+    if i == 5:
+        break
+print("코드 종료")
