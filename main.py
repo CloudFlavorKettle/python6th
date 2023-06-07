b961d5fb3a9ac711dd43c8b3580261b5f1126508
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-07 09:33:10 +0900

    if 실습

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-print("He said, \"Hello World\"")
-print('He said, "Hello World"')
-print('It\'s beautiful day')
-print("It's beautiful day")
+# x = 5
+# if x > 3:
+#     print("x는 3보다 크다.")
+# 
+# age = 18
+# if age >= 18:
+#     print("성인입니다.")
+# else:
+#     print("미성년자입니다.")
+
+# score = 85
+# if score >= 90:
+#     print("A 학점")
+# else:
+#     if score >= 80:
+#         print("B 학점")
+#     else:
+#         if score >= 70:
+#             print("C 학점")
+#         else:
+#             print("D 학점")
+
+# a = int(input("Enter Number Greater then or equal to 2:"))
+# if a >= 3:
+#     print("Correct!! You have Entered: ", a)
+# else:
+#     print("Wrong!! You have Entered: ", a)
+
+day = input("요일을 입력하세요: ")
+if day == "Mon":
+    print("오늘은 월요일")
+elif day == "Tue":
+    print("화요일")
+elif day == "Wed":
+    print("수요일")
+else:
+    print("휴일")
