956252217d941aea047a11c670554193b54bebf4
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-07 11:22:55 +0900

    array 실습

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-# for in
-st = "멋쟁이 사자"
-for ch in st:
-    print(ch)
-else:
-    print("Else")
+import array
+str_roll = array.array('i', [101, 102, 103, 104, 105])
+print(str_roll[0])
+print(str_roll[1])
+print(str_roll[2])
+print(str_roll[3])
+print(str_roll[4])
 
-print("코드 종료")
+for element in str_roll:
+    print(element)
+
+print("인덱스를 이용한 순회")
+n = len(str_roll)
+for i in range(n):
+    print(i, "=", str_roll[i])
+
+print("인덱스를 사용한 while 루프 배열 순회")
+i = 0
+while i < n:
+    print(str_roll[i])
+    i += 1
