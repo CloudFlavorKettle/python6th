3436fd31bd96ecd13646f7c3372da68337713f1f
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-07 10:39:01 +0900

    for in 실습

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-for i in range(5):
-    print(i)
+# for in
+st = "멋쟁이 사자"
+for ch in st:
+    print(ch)
+else:
+    print("Else")
 
-for i in range(1, 10, 2):
-    print(1)
-
-for i in range(-1, -10, -2):
-    print(i)
-
-print("Reverse Rage with Start, stop, step")
-r = range(5, 0, -1)
-print(r[0])
-print(r[1])
-print(r[2])
-print(r[3])
-print(r[4])
+print("코드 종료")
