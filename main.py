22bb1466bf4437510db4350546778db4b765efc1
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-07 13:25:54 +0900

    배열의 슬라이싱 실습

--- main.py
+++ main.py
@@ -1,54 +1,33 @@
 from array import *
 
 stu_roll = array('i', [101, 102, 103, 104, 105])
-# n = len(stu_roll)
-# i = 0
-# while i < n:
-#     print(stu_roll[i])
-#     i += 1
 
-# print("Array After Insert")
-# stu_roll.insert(1, 106)
-# stu_roll.insert(3, 107)
-# n = len(stu_roll)
-# i = 0
-# while i <n:
-#     print(stu_roll[i])
-#     i += 1
-
-# print("배열 요소 삭제")
-#
-# stu_roll.remove(101)
-# n = len(stu_roll)
-# i = 0
-# while i < n:
-#     print(stu_roll[i])
-#     i += 1
-
-print("배열 pop() 함수 실습")
-
-stu_roll.pop(1)
+print("배열 슬라이싱")
 n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+for i in range(n):
+    print(i, "=", stu_roll[i])
 
-print("array index 메소드")
-print(stu_roll.index(101))
+print("1:5까지")
+a= stu_roll[1:5]
+for i in a:
+    print(i)
 
-print("exxtend() 메소드")
-arr = array('i', [201, 208, 209])
-stu_roll.extend(arr)
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+print("0부터 끝까지")
+b = stu_roll[0:]
+for i in b:
+    print(i)
 
-stu_roll.reverse()
-n = len(stu_roll)
-i = 0
-while i < n:
-    print(stu_roll[i])
-    i += 1
+print("처음부터 5번째까지")
+c = stu_roll[:5]
+for i in c:
+    print(i)
+
+print("마지막 요소 4개 출력")
+d = stu_roll[-4:]
+for i in d:
+    print(i)
+
+print("0부터 6번째 까지 2개씩 건너뛰어 출력")
+e = stu_roll[0:7:2]
+for i in e:
+    print(i)
