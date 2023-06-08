5e181de4b05a87e91e98aee00d504de25371f0de
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-08 14:03:20 +0900

    튜플 실습

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
-def generate_alophabet(start_latter, end_latter):
-    start = ord(start_latter)
-    end = ord(end_latter)
-    while start <= end:
-        yield chr(start)
-        start += 1
+b = (10)
+c = (10,)
 
-runner = generate_alophabet('A', 'F')
+print(type(b))
+print(type(c))
 
-for letter in runner:
-    print(letter)
+d = (10, 20, 30, 40)
+e = (10, 20, -50, 21.3, '멋쟁이사자')
+f = 10, 20, -50, 21.3, '멋쟁이사자'
+
+print(d, e, f, sep='\n')
+
+print(f[1])
+print(f[2])
+print(f[3])
+print(f[4])
+
+print(f[1:4])
+
+print((f[3:]))
+g = c + f
+print(g)
+print(f * 5)
+print(-10 in f)
+print("=======")
+h = (10, 20, -50, 21)
+print(min(h), max(h))
+
+print(h.count(20))
+print(h.index(20))
+sorted_a = sorted(h)
+print(sorted_a)
+a = (10, 20, -50)
+x, y, z = a
+print(x, y, z)
+
+a = 1
+b = 2
+
+a, b = b, a
+
+print(a, b)
+
+list_h = list(h)
+print(list_h, type(list_h))
+
+tuple_h = tuple(list_h)
+
+print(tuple_h, type(tuple_h))
+
+nested_tuple = ((1,2,3),(4,5,6),(7,8,9))
+print(nested_tuple)
