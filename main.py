175c09dd8a13b538b7b1dfffc41375bdeba7ff33
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-09 13:24:59 +0900

    파일 실습 (열기, 닫기)

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from datetime import timedelta, datetime, date
-td = timedelta(days=10)
-print(td)
+file_object = open('example.txt', 'r')
 
-d1 = date(year=2023,month=5, day=5)
-d2 = date(year=2023,month=6, day=9)
+content = file_object.read()
 
-#날짜의 연산자 오버로딩으로 비교할 수 있습니다.
-print(d1 == d2)
-print(d1 < d2)
-print(d1 > d2)
+print(content)
 
-dt = datetime.today()
-
-formatted_datetime = dt.strftime('%B,%d,%y')
-print(formatted_datetime)
+file_object.close()
