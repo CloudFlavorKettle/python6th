c11d6e0e79ebe5ec5c6b3aaf2bcc37c728ce5fe0
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-06-09 14:19:24 +0900

    여러줄 쓰기

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-with open('example.txt', 'w') as file_object:
-    content = """This is a multiline string.
-    Python is a versatile language.
-    It is easy to learn and use."""
-    print(content)
-    file_object.write(content)
+# import os
+#
+# filename = 'example.txt'
+#
+# print("파일이 존재하는지 확인하기")
+# if os.path.isfile(filename):
+#     print(f"{filename}이 존재합니다.")
+# else:
+#     print(f"{filename}이 없습니다.")
+
+file_object = open('list_example', 'w')
+
+content_list = ["Python", "Java", "C++", "Javascript"]
+
+for item in content_list:
+    file_object.write(item + '\n')
+
+file_object.close()
